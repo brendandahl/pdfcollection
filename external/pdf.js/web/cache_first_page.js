2b25f89ba41c7ed776112ba3b7527c4f6623af4e
Brendan Dahl <brendan.dahl@gmail.com>
2015-09-11 14:59:10 -0700

    Initial import of pdf collection.

---
+++ external/pdf.js/web/cache_first_page.js
@@ -0,0 +1,25 @@
+if (!HTMLCanvasElement.prototype.toBlob) {
+ Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+  value: function (callback, type, quality) {
+
+    var binStr = atob( this.toDataURL(type, quality).split(',')[1] ),
+        len = binStr.length,
+        arr = new Uint8Array(len);
+
+    for (var i=0; i<len; i++ ) {
+     arr[i] = binStr.charCodeAt(i);
+    }
+
+    callback( new Blob( [arr], {type: type || 'image/png'} ) );
+  }
+ });
+}
+
+document.addEventListener('pagerendered', function (e) {
+  var pageNumber = e.detail.pageNumber;
+  if (pageNumber !== 1) {
+    return;
+  }
+  var canvas = document.getElementById('page' + pageNumber);
+  
+}, true);
